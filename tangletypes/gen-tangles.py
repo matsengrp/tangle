0ea9cb83b25e0738de6910e76afc595d3f850558
Erick Matsen <matsen@fhcrc.org>
2014-10-06 18:43:56 -0700

    Validation strings for tangles.

--- tangletypes/gen-tangles.py
+++ tangletypes/gen-tangles.py
@@ -14,7 +14,7 @@ for n in (int(a) for a in sys.argv[1:]):
                          range(len(trees)))
         return t_idx
 
-    tangles = make_tangles(n, symmetric=False, verbose=False)
+    tangles = make_tangles(n, symmetric=True, verbose=False)
     sage.structure.sage_object.save(
         list(saveable_tangle(*x) for x in tangles),
         filename=("tangle"+str(n)))
@@ -22,11 +22,16 @@ for n in (int(a) for a in sys.argv[1:]):
         for x in tangles:
             f.write(to_newick_pair(*x)+"\n")
     with open("tangle"+str(n)+".idx", "w") as f:
-        for (t1, t2, mu) in tangles:
+        for (t1, t2, coset) in tangles:
             t2p = t2.copy()
-            t2p.relabel(coset_dict(t1, t2, mu))
-            f.write("{}\t{}\t{}\n".format(
-                find_tree(t1),
-                find_tree(t2p),
-                size(mu)))
+            t2p.relabel(coset_dict(t1, t2, coset))
+            t1_idx = find_tree(t1)
+            t2p_idx = find_tree(t2p)
+            newick_pair_str = "{}\t{}".format(to_newick(t1), to_newick(t2p))
+            assert(to_newick_pair(t1, t2, coset) == newick_pair_str)
+            f.write("\t".join([str(o) for o in [
+                t1_idx,
+                t2p_idx,
+                newick_pair_str,
+                size(coset)]])+"\n")
     print "{}\t{}".format(n, time.time() - old_time)
