978dff89ddcf63e118c8aafbe009e2115b1421cc
Erick Matsen <matsen@fhcrc.org>
2014-10-07 07:01:53 -0700

    Now counting isom classes of t_1.

--- tangletypes/gen-tangles.py
+++ tangletypes/gen-tangles.py
@@ -2,6 +2,7 @@
 
 import time
 from sage.all import *
+from collections import Counter
 load("../curvature/tree-fun.py")
 load("../tangle-fun.py")
 
@@ -14,6 +15,12 @@ for n in (int(a) for a in sys.argv[1:]):
                          range(len(trees)))
         return t_idx
 
+    (map_to_class, _) = equivalence_classes(rooted_is_isomorphic, trees)
+    tally = Counter()
+    for rep_idx in map_to_class:
+        tally[rep_idx] += 1
+    isom_count = [tally[map_to_class[i]] for i in range(len(map_to_class))]
+
     tangles = make_tangles(n, symmetric=True, verbose=False)
     sage.structure.sage_object.save(
         list(saveable_tangle(*x) for x in tangles),
@@ -24,7 +31,7 @@ for n in (int(a) for a in sys.argv[1:]):
     with open("tangle"+str(n)+".idx", "w") as f:
         for (t1, t2, coset) in tangles:
             t2p = t2.copy()
-            t2p.relabel(coset_dict(t1, t2, coset))
+            t2p.relabel(symmetric_group_dict(t1, t2, representative(coset)))
             t1_idx = find_tree(t1)
             t2p_idx = find_tree(t2p)
             newick_pair_str = "{}\t{}".format(to_newick(t1), to_newick(t2p))
@@ -33,5 +40,6 @@ for n in (int(a) for a in sys.argv[1:]):
                 t1_idx,
                 t2p_idx,
                 newick_pair_str,
-                size(coset)]])+"\n")
+                isom_count[t1_idx]
+                ]])+"\n")
     print "{}\t{}".format(n, time.time() - old_time)
