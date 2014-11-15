169711ba7e5f69348eee841f0bec08dd0b0ea124
Erick Matsen <matsen@fhcrc.org>
2014-11-15 10:10:02 -0800

    Less terrible CLI for gen-tangles.

--- gen-tangles.py
+++ gen-tangles.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env sage
 
-from subprocess import check_output
+import argparse
 from sys import stdout
 from sage.all import *
 load("curvature/tree-fun.py")
 load("tangle-fun.py")
 
-for n in (int(a) for a in sys.argv[1:]):
-    tangles_extras = make_tangles_extras(n, symmetric=True)
-    tangles = [extra[0] for extra in tangles_extras]
-    tangle_base = "tangle{}".format(n)
-    sage.structure.sage_object.save(
-        [saveable_tangle(*x) for x in tangles],
-        filename=tangle_base)
-    newick_set = set(to_newick_pair(*x) for x in tangles)
-    assert(len(tangles) == len(newick_set))
-    with open(tangle_base+".idx", "w") as f:
-        print "-"*len(tangles)
-        for (x, t1_idx, t2p_idx) in tangles_extras:
-            f.write("\t".join([str(o) for o in [
-                t1_idx,
-                t2p_idx,
-                to_newick_pair(*x),
-                "".join(str(x[2]).split())  # Cosets with no whitespace.
-                ]])+"\n")
-            stdout.write("*")
-            stdout.flush()
-        print ""
-    # Print an enumeration of trees so we can make sense of the .idx file
-    trees = enumerate_rooted_trees(n)
-    with open("tree{}.tre".format(n), "w") as f:
-        for t in trees:
-            f.write(to_newick(t) + "\n")
+parser = argparse.ArgumentParser(description='Generate tangles',
+                                 prog='gen-tangles.py')
+
+parser.add_argument('n', type=int, help='How many leaves')
+parser.add_argument('--asymmetric', action='store_true',
+                    help='Generate tangles without exchange symmetry.')
+
+args = parser.parse_args()
+n = args.n
+
+tangles_extras = make_tangles_extras(n, not args.asymmetric)
+tangles = [extra[0] for extra in tangles_extras]
+tangle_base = "tangle{}".format(n)
+sage.structure.sage_object.save(
+    [saveable_tangle(*x) for x in tangles],
+    filename=tangle_base)
+newick_set = set(to_newick_pair(*x) for x in tangles)
+assert(len(tangles) == len(newick_set))
+with open(tangle_base+".idx", "w") as f:
+    print "-"*len(tangles)
+    for (x, t1_idx, t2p_idx) in tangles_extras:
+        f.write("\t".join([str(o) for o in [
+            t1_idx,
+            t2p_idx,
+            to_newick_pair(*x),
+            "".join(str(x[2]).split())  # Cosets with no whitespace.
+            ]])+"\n")
+        stdout.write("*")
+        stdout.flush()
+    print ""
+# Print an enumeration of trees so we can make sense of the .idx file
+trees = enumerate_rooted_trees(n)
+with open("tree{}.tre".format(n), "w") as f:
+    for t in trees:
+        f.write(to_newick(t) + "\n")
