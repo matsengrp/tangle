d3b3a4635cde9b5d952bb96648a67f17911167a7
Erick Matsen <matsen@fhcrc.org>
2014-12-22 09:18:31 -0800

    Figure and text revision.

--- prep_figures/plot-count.py
+++ prep_figures/plot-count.py
@@ -2,7 +2,7 @@
 
 import ggplot as gg
 from pandas import DataFrame, melt
-
+import scipy
 
 def n_trees_f(n):
     def aux(k):
@@ -12,27 +12,37 @@ def n_trees_f(n):
             return 1
     return aux(2*n - 3)
 
-sizes = range(3, 9)
 
-n_tangles = []
-n_trees = [n_trees_f(i) for i in sizes]
+def plot_count(symmetry, max_size):
+    sizes = range(3, max_size+1)
+    n_trees = [n_trees_f(i) for i in sizes]
+    n_tangles = []
 
-for i in sizes:
-    with open('../rooted-symmetric/tangle{}.idx'.format(i), 'r') as trees:
-        n_tangles.append(len(list(trees)))
+    for i in sizes:
+        with open('../rooted-{0}/tangle{1}.idx'.format(symmetry, i), 'r') as trees:
+            n_tangles.append(len(list(trees)))
 
-df = DataFrame({'sizes': sizes, 'n_tangles': n_tangles, 'n_trees': n_trees})
+    df = DataFrame(
+        {'sizes': sizes, 'n_tangles': n_tangles, 'n_trees': n_trees})
 
-molten = melt(df, id_vars=['sizes'])
-print(molten)
+    if symmetry == 'symmetric':
+        df['naive'] = [scipy.special.binom(n, 2) for n in n_trees]
+    else:
+        df['naive'] = [n*n for n in n_trees]
 
+    molten = melt(df, id_vars=['sizes'])
+    print(molten)
 
-theme = gg.theme_seaborn()
-theme._rcParams['axes.labelsize'] = 'x-large'
-theme._rcParams['legend.fancybox'] = 'False'
-theme._rcParams['xtick.labelsize'] = 'x-large'
-theme._rcParams['ytick.labelsize'] = 'x-large'
-p = gg.ggplot(gg.aes(x='sizes', y='value', color='variable'), data=molten)
-p = p + gg.geom_line() + gg.scale_y_log10() + theme
+    theme = gg.theme_seaborn()
+    theme._rcParams['font.size'] = 16
+    theme._rcParams['axes.labelsize'] = 'x-large'
+    theme._rcParams['legend.fancybox'] = 'False'
+    theme._rcParams['xtick.labelsize'] = 'x-large'
+    theme._rcParams['ytick.labelsize'] = 'x-large'
+    p = gg.ggplot(gg.aes(x='sizes', y='value', color='variable'), data=molten)
+    p = p + gg.geom_line() + gg.scale_y_log10() + theme
 
-gg.ggsave(p, 'count.svg')
+    gg.ggsave(p, symmetry+'-count.svg')
+
+plot_count('symmetric', 8)
+plot_count('asymmetric', 7)
