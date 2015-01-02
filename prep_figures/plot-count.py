3d99a54a7f377a941818213fa58000fbe7f8d08c
Erick Matsen <matsen@fhcrc.org>
2015-01-02 15:41:21 -0800

    Moving to pandas plotting.

--- prep_figures/plot-count.py
+++ prep_figures/plot-count.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-import ggplot as gg
-from pandas import DataFrame, melt
-import scipy
+from pandas import DataFrame
+from scipy.special import binom
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+plt.rcParams['font.size'] = 15
+
 
 def n_trees_f(n):
     def aux(k):
@@ -26,23 +29,12 @@ def plot_count(symmetry, max_size):
         {'sizes': sizes, 'n_tangles': n_tangles, 'n_trees': n_trees})
 
     if symmetry == 'symmetric':
-        df['naive'] = [scipy.special.binom(n, 2) for n in n_trees]
+        df['naive'] = [binom(n, 2) for n in n_trees]
     else:
         df['naive'] = [n*n for n in n_trees]
 
-    molten = melt(df, id_vars=['sizes'])
-    print(molten)
-
-    theme = gg.theme_seaborn()
-    theme._rcParams['font.size'] = 16
-    theme._rcParams['axes.labelsize'] = 'x-large'
-    theme._rcParams['legend.fancybox'] = 'False'
-    theme._rcParams['xtick.labelsize'] = 'x-large'
-    theme._rcParams['ytick.labelsize'] = 'x-large'
-    p = gg.ggplot(gg.aes(x='sizes', y='value', color='variable'), data=molten)
-    p = p + gg.geom_line() + gg.scale_y_log10() + theme
-
-    gg.ggsave(p, symmetry+'-count.svg')
+    p = df.plot(x='sizes', logy=True)
+    p.get_figure().savefig(symmetry+'-count.svg')
 
 plot_count('symmetric', 8)
 plot_count('asymmetric', 7)
