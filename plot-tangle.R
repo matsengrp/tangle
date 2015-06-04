6ed96f0c7903900fb33d6205b41a52b5a44d4d46
Erick Matsen <matsen@fredhutch.org>
2015-06-04 11:56:07 -0700

    Improved plot-tangle.R.

--- plot-tangle.R
+++ plot-tangle.R
@@ -5,7 +5,10 @@ library(dendextend)
 
 args <- commandArgs(TRUE)
 
-stopifnot(length(args) %in% c(3, 4))
+if(!(length(args) %in% c(3, 4))) {
+    cat("Usage: idx_file which_tangle_zero_idx out_file\n")
+    stop()
+}
 
 idx_file <- args[1]
 which_tangle_zero_idx <- args[2]
@@ -14,7 +17,7 @@ out_file <- args[3]
 plot_fun <- ifelse(
         length(args) == 4,
         function(x) plot(x, use.edge.length = FALSE, main = args[4], axes=FALSE, leaflab="none"),
-        function(x) plot(x, use.edge.length = FALSE, leaflab="none"))
+        function(x) plot(x, use.edge.length = FALSE, axes=FALSE, leaflab="none"))
 
 
 df <- read.table(idx_file, stringsAsFactors=FALSE)
@@ -24,6 +27,8 @@ t2 <- df[w, 4]
 
 dendrogram_of_newick <- function(s) {
   t <- read.tree(text=s)
+  if(!(is.rooted(t)))
+    t <- root(t, 1, resolve.root=TRUE)
   t$edge.length <- 1
   as.dendrogram(as.hclust(chronos(t)))
 }
